ab87d5c2f3fb9fdc88ed2127cf14aa4734626ef3
Jeet Sadhukhan <jeet.sadhukhan9.2@gmail.com>
2025-10-31 20:52:28 +0530

    Update pattern.py
    2 more pattern added

--- pattern.py
+++ pattern.py
@@ -27,3 +27,34 @@ def square_char(n):
 n=int(input("Enter the number"))
 square_pattern(n)
 square_char(n)
+
+"""
+1 2 3
+4 5 6
+7 8 9
+"""
+
+def square_pat(n):
+    x=0
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            x+=1
+            print(x," ",end="")
+        print("\n",end="")
+
+print(square_pat(n))
+
+"""
+*
+* *
+* * *
+* * * *
+"""
+
+def tri(n):
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            print("* ",end="")
+        print("\n",end="")
+print(tri(n))
+
