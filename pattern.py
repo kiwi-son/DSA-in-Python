92057e7e3816be93be058c571f533225525bb876
Jeet Sadhukhan <jeet.sadhukhan9.2@gmail.com>
2025-11-01 20:47:43 +0530

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -70,3 +70,52 @@ def num_tri(n):
             print(i," ",end="")
         print("\n",end="")
 num_tri(n)
+
+"""
+1
+1 2
+1 2 3
+1 2 3 4
+"""
+
+def num_tri2(n):
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            print(j," ", end="")
+        print("\n",end="")
+
+num_tri2(n)
+"""
+1
+2  1
+3  2  1
+4  3  2  1
+"""
+def rev_tri(n):
+    for i in range(1,n+1):
+        for j in range(i,0,-1):
+            print(j," ", end="")
+        print("\n",end="")
+
+
+rev_tri(n)
+
+def floyed_tri(n):
+    x=0
+    for i in range(n):
+        for j in range(0,i+1):
+            x+=1
+            print(x," ",end="")
+        print("\n",end="")
+
+floyed_tri(n)
+
+def invert(n):
+    for i in range(1,n+1):
+        for k in range(i-1):
+            print("   ",end="")
+        for j in range(n+1-i,0,-1):
+            print(i," ",end="" )
+        print("\n",end="")
+
+invert(n)
